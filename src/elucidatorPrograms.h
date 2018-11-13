1f162459d195b6ad82cab7fcc5025c99b7a5b276
Nicholas Hathaway <nickjhathaway@gmail.com>
2018-11-12 22:49:03 -0500

    code move;

--- src/elucidatorPrograms.h
+++ src/elucidatorPrograms.h
@@ -35,3 +35,54 @@
 #include "elucidatorPrograms/seqSearching.hpp"
 
 
+#include <njhcpp/progutils/oneRing.hpp>
+
+#include <seqServerPrograms.h>
+#include <SeekDeepPrograms.h>
+#include <TwoBitPrograms.h>
+#include <njhseq/ProgramRunners.h>
+
+namespace njhseq {
+
+int testFunc(const njh::progutils::CmdArgs & inputCommands) {
+	seqSetUp setUp(inputCommands);
+	setUp.finishSetUp(std::cout);
+	std::cout << "This is a test!" << std::endl;
+	return 0;
+}
+
+
+class elucidatorRunner: public njh::progutils::OneRing {
+public:
+	elucidatorRunner();
+};
+//,createReadConsensusRandomPicking  BamExtractPathawaysFromRegion
+elucidatorRunner::elucidatorRunner() :
+		njh::progutils::OneRing(
+				{ addRing<pacbioExpRunner>(),      addRing<genExpRunner>(),
+					addRing<kmerExpRunner>(),        addRing<clusteringExpRunner>(),
+					addRing<fileFormatExpRunner>(),
+					addRing<bamExpRunner>(),         addRing<bamCountingExpRunner>(),
+					addRing<programWrapperRunner>(),
+					addRing<seqUtilsTrimRunner>(),
+					addRing<seqUtilsModRunner>(),
+					addRing<gffExpRunner>(),   			 addRing<bedExpRunner>(),
+					addRing<geneExpRunner>(),        addRing<SeqServerRunner>(),
+					addRing<SeekDeepRunner>(),       addRing<TwoBit::TwoBitRunner>(),
+					addRing<repelinRunner>(),        addRing<ampliconAnalysisRunner>(),
+					addRing<readSimulatorRunner>(),  addRing<graphicsUtilsRunner>(),
+					addRing<miscRunner>(),           addRing<printInfoRunner>(),
+					addRing<seqUtilsRunner>(),       addRing<seqUtilsConverterRunner>(),
+					addRing<seqUtilsExtractRunner>(),addRing<seqUtilsInfoRunner>(),
+					addRing<simulatorRunner>(),      addRing<parsingFileExpRunner>(),
+					addRing<benchMarkingRunner>(),   addRing<jsonExpRunner>(),
+					addRing<ManipulateTableRunner>(),
+					addRing<metaExpRunner>(),
+					addRing<seqSearchingRunner>(),
+					addRing<pairProcessingRunner>(),
+				},//
+				{ addFunc("testFunc", testFunc, false)
+				}, "elucidator", "1", "0", "0-dev") {
+}
+
+}  // namespace njhseq
