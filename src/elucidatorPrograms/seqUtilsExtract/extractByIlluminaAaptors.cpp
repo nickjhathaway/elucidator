ab0536c523355c926b5c8e20df7a993a4f495941
Nick Hathaway <nickjhathaway@gmail.com>
2019-11-29 21:48:17 -0500

    added sample barcode determination;

--- src/elucidatorPrograms/seqUtilsExtract/extractByIlluminaAaptors.cpp
+++ src/elucidatorPrograms/seqUtilsExtract/extractByIlluminaAaptors.cpp
@@ -56,6 +56,9 @@ struct ProcessedForIlluminaAdaptorRes{
 	uint32_t rBarPosEnd_ = 0;
 
 
+
+
+
 };
 
 struct ProcessedForIlluminaAdaptorPars{
@@ -86,9 +89,7 @@ ProcessedForIlluminaAdaptorRes processSeqForAdaptors(seqInfo & seq,
 		//check the forward primer
 		if('-' != frontSeqAdaptorInfo.seqAln_.seq_.back() && ret.fBarPosStart_ > 4 ){
 			ret.fprimerEnd_= getRealPosForAlnPos(frontSeqAdaptorInfo.seqAln_.seq_, frontSeqAdaptorInfo.primerAln_.seq_.find_last_not_of("-")) + 1;
-			if(ret.fprimerStart_ > 0){
-				ret.fwdPass_ = true;
-			}
+			ret.fwdPass_ = true;
 		}
 	}
 
@@ -102,7 +103,7 @@ ProcessedForIlluminaAdaptorRes processSeqForAdaptors(seqInfo & seq,
 			if(backSeqAdaptorInfo.seqAln_.seq_.back() != '-'){
 				ret.rprimerEnd_= getRealPosForAlnPos(backSeqAdaptorInfo.seqAln_.seq_, backSeqAdaptorInfo.primerAln_.seq_.find_last_not_of("-")) + 1;
 			}
-			ret.rprimerStart_ = getRealPosForAlnPos(backSeqAdaptorInfo.seqAln_.seq_, backSeqAdaptorInfo.primerAln_.seq_.find_first_not_of("-")) + 1;
+			ret.rprimerStart_ = getRealPosForAlnPos(backSeqAdaptorInfo.seqAln_.seq_, backSeqAdaptorInfo.primerAln_.seq_.find_first_not_of("-"));
 			ret.rBarPosStart_ = getRealPosForAlnPos(backSeqAdaptorInfo.seqAln_.seq_, firstN_AlnPos);
 			ret.rBarPosEnd_ = getRealPosForAlnPos(backSeqAdaptorInfo.seqAln_.seq_, lastN_AlnPos) + 1;
 			if( ((seq.seq_.size() - startOfBackSeq) - ret.rBarPosEnd_) > 4){
@@ -118,7 +119,7 @@ ProcessedForIlluminaAdaptorRes processSeqForAdaptors(seqInfo & seq,
 	if(ret.fwdPass_ && ret.revPass_){
 		if(pars.keepPrimer_){
 			if(!pars.primerUpper_){
-				changeSubStrToLower(seq.seq_, ret.fprimerStart_, ret.fprimerEnd_ - ret.fprimerEnd_);
+				changeSubStrToLower(seq.seq_, ret.fprimerStart_, ret.fprimerEnd_ - ret.fprimerStart_);
 				changeSubStrToLower(seq.seq_, ret.rprimerStart_, ret.rprimerEnd_ - ret.rprimerStart_);
 			}
 		}
@@ -130,19 +131,87 @@ ProcessedForIlluminaAdaptorRes processSeqForAdaptors(seqInfo & seq,
 int seqUtilsExtractRunner::extractByIlluminaAaptors(const njh::progutils::CmdArgs & inputCommands) {
 	std::string illuminaForward = "AATGATACGGCGACCACCGAGATCTACACNNNNNNNNACACTCTTTCCCTACACGACTCGCCAAGCTGA";
 	std::string illuminaReverse = "CAAGCAGAAGACGGCATACGAGATNNNNNNNNGTGACTGGAGTTCAGACGTGTGCTCTTCCGAT";
-	double identityCutOff = .70;
+	double primerIdentityCutOff = .70;
+	double barIdentityCutOff = .60;
 	ProcessedForIlluminaAdaptorPars extractPars;
+	bfs::path illuminaBarcodeSampleSheet = "";
+
 	seqUtilsExtractSetUp setUp(inputCommands);
 	setUp.setOption(extractPars.primerUpper_, "--primerUpper", "When keeping primer, keep upper case");
 	setUp.setOption(extractPars.keepPrimer_, "--keepPrimer", "Keep Primer sequence, otherwise remove it");
 
-	setUp.setOption(identityCutOff, "--identityCutOff", "Percent Identity Cut Off for matching the primers");
+	setUp.setOption(barIdentityCutOff, "--barIdentityCutOff", "Bar Identity Cut Off");
+	setUp.setOption(primerIdentityCutOff, "--primerIdentityCutOff", "Percent Identity Cut Off for matching the primers");
+	setUp.setOption(illuminaBarcodeSampleSheet, "--illuminaBarcodeSampleSheet", "Illumina Barcode Sample Sheet", true);
+
 	setUp.processVerbose();
 	setUp.processDebug();
 	setUp.processReadInNames(true);
 	setUp.processDirectoryOutputName(true);
 	setUp.finishSetUp(std::cout);
 	setUp.startARunLog(setUp.pars_.directoryName_);
+
+
+	//read in barcodes
+	//check for same barcodes
+	table barcodesTab(illuminaBarcodeSampleSheet, "\t", true);
+	barcodesTab.checkForColumnsThrow(VecStr{"sample_name", "fw", "rev"}, __PRETTY_FUNCTION__);
+	struct IlluminaDualBarCode{
+		IlluminaDualBarCode(const std::string & samp, const std::string & fwBar5_3,
+				const std::string & revBar5_3) :
+				sampleName_(samp), fwBar_("fw", fwBar5_3), revBar_("rev", revBar5_3) {
+
+		}
+		std::string sampleName_;
+		seqInfo fwBar_;
+		seqInfo revBar_;
+	};
+
+	std::vector<IlluminaDualBarCode> barcodes;
+
+	uint64_t maxBarLen = 0;
+
+	for(const auto & row : barcodesTab){
+		auto bar = IlluminaDualBarCode{
+			row[barcodesTab.getColPos("sample_name")],
+			row[barcodesTab.getColPos("fw")],
+			row[barcodesTab.getColPos("rev")]};
+		//check other barcodes for same pairing and for same sample name
+		for(const auto & prevBar : barcodes){
+			if(bar.sampleName_ == prevBar.sampleName_){
+				std::stringstream ss;
+				ss << __PRETTY_FUNCTION__ << ", error " << "already have sample name: " << bar.sampleName_<< "\n";
+				throw std::runtime_error{ss.str()};
+			}
+			if (bar.fwBar_.seq_ == prevBar.fwBar_.seq_
+					&& bar.revBar_.seq_ == prevBar.revBar_.seq_) {
+				std::stringstream ss;
+				ss << __PRETTY_FUNCTION__ << ", error "
+						<< "already have barcode pairing: fw: " << bar.fwBar_.seq_
+						<< " rev: " << bar.revBar_.seq_ << " for sample: "
+						<< prevBar.sampleName_ << "\n";
+				throw std::runtime_error { ss.str() };
+			}
+		}
+		readVec::getMaxLength(bar.fwBar_, maxBarLen);
+		readVec::getMaxLength(bar.revBar_, maxBarLen);
+
+		barcodes.emplace_back(bar);
+	}
+	//make outputs
+	auto fastqDir = njh::files::make_path(setUp.pars_.directoryName_, "fastq");
+	njh::files::makeDir(njh::files::MkdirPar{fastqDir});
+	MultiSeqOutCache<seqInfo> outputs;
+	for(const auto & bar : barcodes){
+		outputs.addReader(bar.sampleName_, SeqIOOptions::genFastqOutGz(njh::files::make_path(fastqDir, bar.sampleName_)));
+	}
+	outputs.addReader("Undetermined", SeqIOOptions::genFastqOutGz(njh::files::make_path(fastqDir, "Undetermined")));
+	//set up for alignment of barcodes for scoring
+	maxBarLen = maxBarLen * 2;
+	gapScoringParameters gapParsBar(5,1,5,1,5,1);
+	auto scoringBar = substituteMatrix::createDegenScoreMatrix(2,-2);
+	aligner alignerBarObj(maxBarLen, gapParsBar, scoringBar, true);
+
 	gapScoringParameters gapPars(5,1,0,0,0,0);
 	gapPars.gapLeftRefOpen_ = 5;
 	gapPars.gapLeftRefExtend_ = 1;
@@ -178,6 +247,8 @@ int seqUtilsExtractRunner::extractByIlluminaAaptors(const njh::progutils::CmdArg
 	writer.openOut();
 
 	std::map<std::string, std::map<std::string, uint32_t>> bestCounts;
+	std::map<std::string, uint32_t> sampleCounts;
+
 	uint32_t smallSeq = 0;
 
 
@@ -245,20 +316,18 @@ int seqUtilsExtractRunner::extractByIlluminaAaptors(const njh::progutils::CmdArg
 			++mismatchDirections;
 			continue;
 		}
-		// add score cut off, possibly 0.60;
-
+		ProcessedForIlluminaAdaptorRes processRes;
 		if(reverseScore > forwardScore){
 			direction = "rev";
 			forwardBar = seqUtil::reverseComplement(fwd3_5_info.barcode_, "DNA");;
 			reverseBar = rev5_3_info.barcode_;
-
 			bool failedIdentity = false;
-			if(rev5_3_info.comp_.distances_.eventBasedIdentity_ < identityCutOff){
+			if(rev5_3_info.comp_.distances_.eventBasedIdentity_ < primerIdentityCutOff){
 				//
 				++failed_rev5_3_Identity;
 				failedIdentity = true;
 			}
-			if(fwd3_5_info.comp_.distances_.eventBasedIdentity_ < identityCutOff){
+			if(fwd3_5_info.comp_.distances_.eventBasedIdentity_ < primerIdentityCutOff){
 				//
 				++failed_fwd3_5_Identity;
 				failedIdentity = true;
@@ -267,7 +336,7 @@ int seqUtilsExtractRunner::extractByIlluminaAaptors(const njh::progutils::CmdArg
 				continue;
 			}
 			uint32_t startOfBackSeq = seq.seq_.size() - 125;
-			auto processRes = processSeqForAdaptors(seq, rev5_3_info, fwd3_5_info, startOfBackSeq, extractPars);
+			processRes = processSeqForAdaptors(seq, rev5_3_info, fwd3_5_info, startOfBackSeq, extractPars);
 			if(!processRes.fwdPass_){
 				++failed_rev5_3_PrimerAln;
 			}
@@ -281,42 +350,17 @@ int seqUtilsExtractRunner::extractByIlluminaAaptors(const njh::progutils::CmdArg
 					seq = seq.getSubRead(processRes.fprimerEnd_, processRes.rprimerStart_- processRes.fprimerEnd_);
 				}
 				seq.reverseComplementRead(false, true);
-				MetaDataInName seqMeta;
-				seqMeta.addMeta("direction", direction);
-				seqMeta.addMeta("fprimerStart", processRes.fprimerStart_);
-				seqMeta.addMeta("forwardBar", forwardBar);
-				seqMeta.addMeta("reverseBar", reverseBar);
-				seqMeta.addMeta("fullBar", njh::pasteAsStr(forwardBar, "+", reverseBar));
-				seqMeta.addMeta("rprimerStart", processRes.rprimerStart_);
-				seq.name_ += seqMeta.createMetaName();
-				writer.write(seq);
-				outPass << seq.name_
-					<< "\t" << direction
-					<< "\t" << fwd5_3_info.comp_.distances_.eventBasedIdentity_
-					<< "\t" << fwd5_3_info.barcode_
-					<< "\t" << rev3_5_info.comp_.distances_.eventBasedIdentity_
-					<< "\t" << rev3_5_info.barcode_
-					<< "\t" << rev5_3_info.comp_.distances_.eventBasedIdentity_
-					<< "\t" << rev5_3_info.barcode_
-					<< "\t" << fwd3_5_info.comp_.distances_.eventBasedIdentity_
-					<< "\t" << fwd3_5_info.barcode_
-					<< std::endl;
-				outBest << seq.name_
-						<< "\t" << direction
-						<< "\t" << forwardBar
-						<< "\t" << reverseBar << std::endl;
 				++passRev;
 			}
 		} else {
 			//process seq
-
 			bool failedIdentity = false;
-			if(fwd5_3_info.comp_.distances_.eventBasedIdentity_ < identityCutOff){
+			if(fwd5_3_info.comp_.distances_.eventBasedIdentity_ < primerIdentityCutOff){
 				//
 				++failed_fwd5_3_Identity;
 				failedIdentity = true;
 			}
-			if(rev3_5_info.comp_.distances_.eventBasedIdentity_ < identityCutOff){
+			if(rev3_5_info.comp_.distances_.eventBasedIdentity_ < primerIdentityCutOff){
 				//
 				++failed_rev3_5_Identity;
 				failedIdentity = true;
@@ -325,7 +369,7 @@ int seqUtilsExtractRunner::extractByIlluminaAaptors(const njh::progutils::CmdArg
 				continue;
 			}
 			uint32_t startOfBackSeq = seq.seq_.size() - 125;
-			auto processRes = processSeqForAdaptors(seq, fwd5_3_info, rev3_5_info, startOfBackSeq, extractPars);
+			processRes = processSeqForAdaptors(seq, fwd5_3_info, rev3_5_info, startOfBackSeq, extractPars);
 			if(!processRes.fwdPass_){
 				++failed_fwd5_3_PrimerAln;
 			}
@@ -338,33 +382,120 @@ int seqUtilsExtractRunner::extractByIlluminaAaptors(const njh::progutils::CmdArg
 				}else{
 					seq = seq.getSubRead(processRes.fprimerEnd_, processRes.rprimerStart_- processRes.fprimerEnd_);
 				}
-				MetaDataInName seqMeta;
-				seqMeta.addMeta("direction", direction);
-				seqMeta.addMeta("fprimerStart", processRes.fprimerStart_);
-				seqMeta.addMeta("forwardBar", forwardBar);
-				seqMeta.addMeta("reverseBar", reverseBar);
-				seqMeta.addMeta("rprimerStart", processRes.rprimerStart_);
-				seqMeta.addMeta("fullBar", njh::pasteAsStr(forwardBar, "+", reverseBar));
-				seq.name_ += seqMeta.createMetaName();
-				writer.write(seq);
-				outPass << seq.name_
-					<< "\t" << direction
-					<< "\t" << fwd5_3_info.comp_.distances_.eventBasedIdentity_
-					<< "\t" << fwd5_3_info.barcode_
-					<< "\t" << rev3_5_info.comp_.distances_.eventBasedIdentity_
-					<< "\t" << rev3_5_info.barcode_
-					<< "\t" << rev5_3_info.comp_.distances_.eventBasedIdentity_
-					<< "\t" << rev5_3_info.barcode_
-					<< "\t" << fwd3_5_info.comp_.distances_.eventBasedIdentity_
-					<< "\t" << fwd3_5_info.barcode_
-					<< std::endl;
-
-				outBest << seq.name_
-						<< "\t" << direction
-						<< "\t" << forwardBar
-						<< "\t" << reverseBar << std::endl;
 				++passFwd;
 			}
+		}
+
+		if(processRes.fwdPass_ && processRes.revPass_){
+			std::string determinedSample = "Undetermined";
+
+			{
+				VecStr fwdBestSample{"Undetermined"};
+				VecStr revBestSample{"Undetermined"};
+				uint32_t bestFwdIds = 0;
+				uint32_t bestFwdAlnScore = 0;
+				uint32_t bestRevIds = 0;
+				uint32_t bestRevAlnScore = 0;
+				seqInfo fwdBarSeq("determined-fwd", forwardBar);
+				seqInfo revBarSeq("determined-rev", reverseBar);
+
+				if(forwardBar.size() < maxBarLen && reverseBar.size() < maxBarLen){
+					for(const auto & bar : barcodes){
+						alignerBarObj.alignCacheGlobal(bar.fwBar_, fwdBarSeq);
+						alignerBarObj.profilePrimerAlignment(bar.fwBar_, fwdBarSeq);
+						if(alignerBarObj.comp_.distances_.eventBasedIdentity_ >=barIdentityCutOff){
+							if(alignerBarObj.comp_.distances_.ref_.identities_ > bestFwdIds){
+								fwdBestSample.clear();
+								bestFwdAlnScore = alignerBarObj.parts_.score_;
+								bestFwdIds = alignerBarObj.comp_.distances_.ref_.identities_;
+								fwdBestSample.emplace_back(bar.sampleName_);
+							}else if(alignerBarObj.comp_.distances_.ref_.identities_ == bestFwdIds){
+								if(alignerBarObj.parts_.score_ > bestFwdAlnScore){
+									fwdBestSample.clear();
+									bestFwdAlnScore = alignerBarObj.parts_.score_;
+									bestFwdIds = alignerBarObj.comp_.distances_.ref_.identities_;
+									fwdBestSample.emplace_back(bar.sampleName_);
+								}else if(alignerBarObj.parts_.score_ == bestFwdAlnScore){
+									fwdBestSample.emplace_back(bar.sampleName_);
+								}
+							}
+						}
+						alignerBarObj.alignCacheGlobal(bar.revBar_, revBarSeq);
+						alignerBarObj.profilePrimerAlignment(bar.revBar_, revBarSeq);
+						if(alignerBarObj.comp_.distances_.eventBasedIdentity_ >=barIdentityCutOff){
+							if(alignerBarObj.comp_.distances_.ref_.identities_ > bestRevIds){
+								revBestSample.clear();
+								bestRevAlnScore = alignerBarObj.parts_.score_;
+								bestRevIds = alignerBarObj.comp_.distances_.ref_.identities_;
+								revBestSample.emplace_back(bar.sampleName_);
+							}else if(alignerBarObj.comp_.distances_.ref_.identities_ == bestRevIds){
+								if(alignerBarObj.parts_.score_ > bestRevAlnScore){
+									revBestSample.clear();
+									bestRevAlnScore = alignerBarObj.parts_.score_;
+									bestRevIds = alignerBarObj.comp_.distances_.ref_.identities_;
+									revBestSample.emplace_back(bar.sampleName_);
+								}else if(alignerBarObj.parts_.score_ == bestRevAlnScore){
+									revBestSample.emplace_back(bar.sampleName_);
+								}
+							}
+						}
+					}
+				}
+				//need one for each and need to match
+				//if multiple in one or both, see if there is only one pair since barcodes can be reused several times
+				if (   njh::in(std::string("Undetermined"),fwdBestSample)
+						|| njh::in(std::string("Undetermined"),revBestSample)
+						|| fwdBestSample.size() == 0
+						|| revBestSample.size() == 0) {
+					determinedSample = "Undetermined";
+				}else{
+					if(fwdBestSample.size() == 1 && revBestSample.size() == 1 && fwdBestSample.front() == revBestSample.front()){
+						determinedSample = fwdBestSample.front();
+					} else {
+						VecStr pairs;
+						for(const auto fwdBest : fwdBestSample){
+							for(const auto & revBest : revBestSample){
+								if(revBest  == fwdBest){
+									pairs.emplace_back(fwdBest);
+									break;
+								}
+							}
+						}
+						if(pairs.size() == 1){
+							determinedSample = pairs.front();
+						}else{
+							determinedSample = "Undetermined";
+						}
+					}
+				}
+			}
+
+			MetaDataInName seqMeta;
+			seqMeta.addMeta("direction", direction);
+			seqMeta.addMeta("fprimerStart", processRes.fprimerStart_);
+			seqMeta.addMeta("forwardBar", forwardBar);
+			seqMeta.addMeta("reverseBar", reverseBar);
+			seqMeta.addMeta("fullBar", njh::pasteAsStr(forwardBar, "+", reverseBar));
+			seqMeta.addMeta("rprimerStart", processRes.rprimerStart_);
+			seqMeta.addMeta("sample", determinedSample);
+			seq.name_ += seqMeta.createMetaName();
+			outputs.add(determinedSample, seq);
+			++sampleCounts[determinedSample];
+			outPass << seq.name_
+				<< "\t" << direction
+				<< "\t" << fwd5_3_info.comp_.distances_.eventBasedIdentity_
+				<< "\t" << fwd5_3_info.barcode_
+				<< "\t" << rev3_5_info.comp_.distances_.eventBasedIdentity_
+				<< "\t" << rev3_5_info.barcode_
+				<< "\t" << rev5_3_info.comp_.distances_.eventBasedIdentity_
+				<< "\t" << rev5_3_info.barcode_
+				<< "\t" << fwd3_5_info.comp_.distances_.eventBasedIdentity_
+				<< "\t" << fwd3_5_info.barcode_
+				<< std::endl;
+			outBest << seq.name_
+					<< "\t" << direction
+					<< "\t" << forwardBar
+					<< "\t" << reverseBar << std::endl;
 		}
 	}
 	OutputStream outBestCounts(njh::files::make_path(setUp.pars_.directoryName_, "bestDirectionCounts.tab.txt"));
@@ -402,6 +533,16 @@ int seqUtilsExtractRunner::extractByIlluminaAaptors(const njh::progutils::CmdArg
 		conditionCount << "totalFailedPrimerAln" << "\t" << totalFailedPrimerAln << "\t" << totalFailedPrimerAln/total << std::endl;
 		conditionCount << "smallSeq" << "\t" << smallSeq << "\t" << smallSeq/total << std::endl;
 		conditionCount << "mismatchDirections" << "\t" << mismatchDirections << "\t" << mismatchDirections/total << std::endl;
+
+	}
+	{
+		OutputStream sampleCountsOut(njh::files::make_path(setUp.pars_.directoryName_, "sampleCounts.tab.txt"));
+		sampleCountsOut << "sample\tcount\tfrac" << std::endl;
+		for(const auto & sampleCount : sampleCounts){
+			sampleCountsOut << sampleCount.first
+					<< "\t" << sampleCount.second
+					<< "\t" << sampleCount.second/static_cast<double>(passFwd + passRev) << std::endl;
+		}
 
 	}
 
