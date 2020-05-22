1be711c6754888e81866e8c4a9211abb7fab8389
Nicholas Hathaway <nickjhathaway@gmail.com>
2020-05-22 16:36:57 -0400

    no message

--- src/elucidatorPrograms/bamExp.cpp
+++ src/elucidatorPrograms/bamExp.cpp
@@ -377,29 +377,7 @@ int bamExpRunner::determineRegionLastz(
 
 
 
-int bamExpRunner::multiBamCoverageFinder(const njh::progutils::CmdArgs & inputCommands){
-	RunCoverageFinderMultiPars covPars;
 
-	seqSetUp setUp(inputCommands);
-	setUp.processVerbose();
-	setUp.processDebug();
-	setUp.setOption(covPars.byBases, "--byBases", "Output coverage in bases rather than reads");
-	setUp.setOption(covPars.regionBatchSize, "--regionBatchSize", "Region Batch Size");
-	setUp.setOption(covPars.coverageCutOff, "--coverageCutOff", "Coverage Cut Off to include region in output");
-	setUp.setOption(covPars.window, "--window", "window size for sliding window");
-	setUp.setOption(covPars.step, "--step", "step size for advancing the window");
-	setUp.setOption(covPars.numThreads, "--numThreads", "Number of threads to use");
-	setUp.processWritingOptions(covPars.outOpts);
-	setUp.setOption(covPars.pat, "--pat", "Pattern in current directory to get coverage for");
-	setUp.setOption(covPars.bams, "--bams", "Either a file with the name of a bam file on each line or a comma separated value of bam file paths");
-
-	setUp.finishSetUp(std::cout);
-
-
-	RunCoverageFinderMulti(covPars);
-
-	return 0;
-}
 
 
 
