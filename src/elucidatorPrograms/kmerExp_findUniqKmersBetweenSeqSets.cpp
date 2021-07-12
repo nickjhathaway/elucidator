61b1739ec2839db4db0f49c29d145b8cbd7608fd
Nicholas Hathaway <nickjhathaway@gmail.com>
2021-07-12 15:58:43 -0400

    counting;

--- src/elucidatorPrograms/kmerExp_findUniqKmersBetweenSeqSets.cpp
+++ src/elucidatorPrograms/kmerExp_findUniqKmersBetweenSeqSets.cpp
@@ -37,24 +37,78 @@
 
 namespace njhseq {
 
+struct CountGenomeKmersPars {
+	CountGenomeKmersPars(uint32_t kmerLength,
+			bool noRevComp, uint32_t numThreads,
+			std::vector<char> allowableCharacters) :
+			kmerLength_(kmerLength), noRevComp_(noRevComp), numThreads_(
+					numThreads),
+					allowableCharacters_(allowableCharacters){
+
+	}
+	CountGenomeKmersPars(){
+
+	}
+	uint32_t kmerLength_{21};
+	bool noRevComp_{false};
+	uint32_t numThreads_{1};
+	std::vector<char> allowableCharacters_{'A', 'C', 'G', 'T'};
+
+	void setOptions(seqSetUp & setUp){
+		setUp.setOption(kmerLength_, "--kmerLength", "kmer Length");
+		setUp.setOption(numThreads_, "--numThreads", "number of threads");
+		setUp.setOption(allowableCharacters_, "--allowableCharacters", "Only count kmers with these allowable Characters");
+		setUp.setOption(noRevComp_, "--noRevComp", "noRevComp");
+	}
+};
+
+std::unordered_map<std::string, uint32_t> countGenomeKmers(const bfs::path & genomeFnp , const CountGenomeKmersPars & pars){
+	std::unordered_map<std::string, uint32_t> ret;
+	SeqInput reader(SeqIOOptions::genFastaIn(genomeFnp));
+	reader.openIn();
+	std::mutex genomeCountsMut;
+	std::function<void()> countKmers =[&reader,&pars,&genomeCountsMut,&ret](){
+		seqInfo seq;
+
+		while(reader.readNextReadLock(seq)){
+			std::unordered_map<std::string, uint32_t> genomeCountsCurrent;
+			if(len(seq) > pars.kmerLength_){
+				for(uint32_t pos = 0; pos < len(seq) - pars.kmerLength_ + 1; ++pos){
+					++genomeCountsCurrent[seq.seq_.substr(pos, pars.kmerLength_)];
+				}
+				if(!pars.noRevComp_){
+					seq.reverseComplementRead(false, true);
+					for(uint32_t pos = 0; pos < len(seq) - pars.kmerLength_ + 1; ++pos){
+						++genomeCountsCurrent[seq.seq_.substr(pos, pars.kmerLength_)];
+					}
+				}
+			}
+			{
+				std::lock_guard<std::mutex> lock(genomeCountsMut);
+				for(const auto & count : genomeCountsCurrent){
+					ret[count.first] += count.second;
+				}
+			}
+		}
+	};
+	njh::concurrent::runVoidFunctionThreaded(countKmers, pars.numThreads_);
+
+	return ret;
+}
+
 int kmerExpRunner::findUniqKmersBetweenSeqSets(const njh::progutils::CmdArgs & inputCommands){
-	bool noRevComp = false;
-	uint32_t kmerLength = 21;
+	CountGenomeKmersPars countPars;
 	bfs::path genomeFnp1 = "";
 	bfs::path genomeFnp2 = "";
 	bool writeOutAllCounts = false;
-	std::vector<char> allowableCharacters{'A', 'C', 'G', 'T'};
+
 	seqSetUp setUp(inputCommands);
 	setUp.processVerbose();
 	setUp.processDebug();
 	setUp.setOption(genomeFnp1, "--genomeFnp1", "genomeFnp1", true);
 	setUp.setOption(genomeFnp2, "--genomeFnp2", "genomeFnp2", true);
-	setUp.setOption(kmerLength, "--kmerLength", "kmer Length");
-	setUp.setOption(allowableCharacters, "--allowableCharacters", "Only count kmers with these allowable Characters");
+	countPars.setOptions(setUp);
 	setUp.setOption(writeOutAllCounts, "--writeOutAllCounts", "writeOutAllCounts");
-	setUp.setOption(noRevComp, "--noRevComp", "noRevComp");
-
-
 	std::string genomeFnp1Base = bfs::basename(njh::files::removeExtension(genomeFnp1));
 	std::string genomeFnp2Base = bfs::basename(njh::files::removeExtension(genomeFnp2));
 
@@ -64,49 +118,12 @@ int kmerExpRunner::findUniqKmersBetweenSeqSets(const njh::progutils::CmdArgs & i
 
 
 
-	std::unordered_map<std::string, uint32_t> genome1Counts;
-	std::unordered_map<std::string, uint32_t> genome2Counts;
 	setUp.rLog_.setCurrentLapName("initial");
 	setUp.rLog_.logCurrentTime("genome1_counting-" + genomeFnp1Base);
-	{
-		seqInfo seq;
-		SeqInput reader(SeqIOOptions::genFastaIn(genomeFnp1));
-		reader.openIn();
-		while(reader.readNextRead(seq)){
-			if(len(seq) > kmerLength){
-
-				for(uint32_t pos = 0; pos < len(seq) - kmerLength + 1; ++pos){
-					++genome1Counts[seq.seq_.substr(pos, kmerLength)];
-				}
-				if(!noRevComp){
-					seq.reverseComplementRead(false, true);
-					for(uint32_t pos = 0; pos < len(seq) - kmerLength + 1; ++pos){
-						++genome1Counts[seq.seq_.substr(pos, kmerLength)];
-					}
-				}
-			}
-		}
-	}
+	std::unordered_map<std::string, uint32_t> genome1Counts = countGenomeKmers(genomeFnp1, countPars);
 	setUp.rLog_.logCurrentTime("genome2_counting-" + genomeFnp2Base);
+	std::unordered_map<std::string, uint32_t> genome2Counts = countGenomeKmers(genomeFnp2, countPars);
 
-	{
-		seqInfo seq;
-		SeqInput reader(SeqIOOptions::genFastaIn(genomeFnp2));
-		reader.openIn();
-		while(reader.readNextRead(seq)){
-			if(len(seq) > kmerLength){
-				for(uint32_t pos = 0; pos < len(seq) - kmerLength + 1; ++pos){
-					++genome2Counts[seq.seq_.substr(pos, kmerLength)];
-				}
-				if(!noRevComp){
-					seq.reverseComplementRead(false, true);
-					for(uint32_t pos = 0; pos < len(seq) - kmerLength + 1; ++pos){
-						++genome2Counts[seq.seq_.substr(pos, kmerLength)];
-					}
-				}
-			}
-		}
-	}
 
 	if(writeOutAllCounts){
 		setUp.rLog_.logCurrentTime("genome1_writing_all-" + genomeFnp1Base);
@@ -124,8 +141,8 @@ int kmerExpRunner::findUniqKmersBetweenSeqSets(const njh::progutils::CmdArgs & i
 	}
 
 
-	std::function<bool(char)> charCheck = [&allowableCharacters](char base){
-		return njh::in(base, allowableCharacters);
+	std::function<bool(char)> charCheck = [&countPars](char base){
+		return njh::in(base, countPars.allowableCharacters_);
 	};
 	setUp.rLog_.logCurrentTime("genome1_unique_deterination-" + genomeFnp1Base);
 
