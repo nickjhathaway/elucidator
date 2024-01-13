dc6f66f1ddb39fffe3ca4ba00b02d0c1f81156b7
Nicholas Hathaway <nickjhathaway@gmail.com>
2024-01-13 17:38:06 -0500

    added plotDomains quarto output summary document; added align best option for finding best kmer match;

--- src/elucidatorPrograms/programWrappers/programWrappers_runnhmmscan.cpp
+++ src/elucidatorPrograms/programWrappers/programWrappers_runnhmmscan.cpp
@@ -19,6 +19,172 @@
 
 namespace njhseq {
 
+std::string plotDomainsQmd = R"(---
+title: "Plotting Domain Hits"
+format:
+  html:
+    theme: cosmo
+    fig-height: 10
+    fig-width: 15
+    fig-align: center
+    toc: true
+    toc-depth: 4 # default is 3
+    toc-title: Contents
+    number-sections: true
+    anchor-sections: true
+    smooth-scroll: true
+    highlight-style: dracula
+    page-layout: full
+    code-fold: true
+    code-tools: true
+    code-link: true
+---
+
+
+```{r setup, echo=FALSE, message=FALSE}
+require(knitr)
+require(DT)
+require(tidyverse)
+require(stringr)
+require(dbscan)
+require(ape)
+require(rwantshue)
+require(tsne)
+require(ggforce)
+require(GGally)
+require(plotly)
+require(heatmaply)
+require(ComplexHeatmap)
+require(fastmatch)
+require(HaplotypeRainbows)
+require(ggtree)
+#turn off messages and warnings and make it so output isn't prefixed by anything,
+#default is to put "##" in front of all output for some reason
+#also set tidy to true so code is wrapped properly
+opts_chunk$set(message=FALSE, warning=FALSE, comment = "", cache = F)
+options(width = 200)
+`%!in%` <- Negate(`%in%`)
+scheme <- iwanthue(seed = 42, force_init = TRUE)
+
+myFormula= x~y
+library(ggpmisc)
+`%!in%` <- Negate(`%in%`)
+
+sofonias_theme = theme_bw() +
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank() )+
+  theme(axis.line.x = element_line(color="black", linewidth = 0.3),axis.line.y =
+          element_line(color="black", linewidth = 0.3))+
+  theme(text=element_text(size=12, family="Helvetica"))+
+  theme(axis.text.y = element_text(size=12))+
+  theme(axis.text.x = element_text(size=12)) +
+   theme(legend.position = "bottom") +
+   theme(plot.title = element_text(hjust = 0.5))
+
+sofonias_theme_xRotate = sofonias_theme +
+  theme(axis.text.x = element_text(size=12, angle = -90, vjust = 0.5, hjust = 0))
+create_dt <- function(x){
+  DT::datatable(x,
+                extensions = 'Buttons',
+                options = list(dom = 'Blfrtip',
+                               buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+                               lengthMenu = list(c(10,25,50,-1),
+                                                 c(10,25,50,"All"))),
+                filter = "top")
+}
+colorPalette_08 = c("#2271B2","#F748A5","#359B73","#F0E442","#D55E00","#3DB7E9","#E69F00","#000000")
+colorPalette_12 = c("#E20134","#FF6E3A","#008DF9","#8400CD","#FFC33B","#9F0162","#009F81","#FF5AAF","#00FCCF","#00C2F9","#FFB2FD","#A40122")
+colorPalette_15 = c("#F60239","#003C86","#EF0096","#9400E6","#009FFA","#008169","#68023F","#00DCB5","#FFCFE2","#FF71FD","#7CFFFA","#6A0213","#008607","#00E307","#FFDC3D")
+
+createColorListFromDf <- function(df, colorPalette = colorPalette_12, iwanthudSeed = rnorm(1) * 100){
+  colorList = list()
+  for (dfColname in colnames(df)) {
+    levels = sort(unique(df[[dfColname]]))
+    scheme <- iwanthue(seed = iwanthudSeed, force_init = TRUE)
+    if (length(levels) <= length(colorPalette_12)) {
+      levelsCols = colorPalette_12[1:length(levels)]
+    } else{
+      levelsCols = scheme$hex(length(levels))
+    }
+    names(levelsCols) = levels
+    colorList[[dfColname]] = levelsCols
+  }
+  return(colorList)
+}
+```
+
+<style type="text/css">
+div.content {
+  max-width: 1800px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
+
+```{r}
+nhmmscan_hits_filtered_merged_noOverlap_table_hits = readr::read_tsv("nhmmscan_hits_filtered_merged_noOverlap_table_hits.tab.txt")
+
+
+nhmmscan_hits_filtered_merged_noOverlap_table_hits_qLens = nhmmscan_hits_filtered_merged_noOverlap_table_hits %>%
+  select(`#chrom`, queryLen) %>%
+  unique() %>%
+  mutate(`#chrom` = factor(`#chrom`),
+         shortName = factor(gsub("\\[.*\\]", "", `#chrom`)))
+
+nhmmscan_hits_filtered_merged_noOverlap_table_hits = nhmmscan_hits_filtered_merged_noOverlap_table_hits %>%
+  mutate(`#chrom` = factor(`#chrom`, levels = levels(nhmmscan_hits_filtered_merged_noOverlap_table_hits_qLens$`#chrom`)))%>%
+  mutate(shortName = factor(gsub("\\[.*\\]", "", `#chrom`), levels = levels(nhmmscan_hits_filtered_merged_noOverlap_table_hits_qLens$shortName) ))
+
+domainPlot = ggplot() +
+  geom_rect(aes(
+    xmin = 0,
+    xmax = queryLen,
+    ymin = as.numeric(shortName) - 0.0,
+    ymax = as.numeric(shortName) + 0.4
+  ),
+  fill = "#2271B2", data = nhmmscan_hits_filtered_merged_noOverlap_table_hits_qLens) +
+
+    geom_rect(aes(
+    xmin = env_from,
+    xmax = env_to,
+    ymin = as.numeric(shortName) - 0.4,
+    ymax = as.numeric(shortName) + 0.4,
+    fill = name,
+    env_from = env_from,
+    env_to = env_to,
+    env_len = env_len,
+    evalue = evalue,
+    hmm_covered = hmm_covered,
+    hmm_From = hmm_From, 
+    hmm_to = hmm_to,
+    hmm_edges = hmm_edges
+
+  ),
+  color = "black",
+  alpha = 0.75,
+   data = nhmmscan_hits_filtered_merged_noOverlap_table_hits) +
+
+  scale_fill_manual(values = scheme$hex(length(nhmmscan_hits_filtered_merged_noOverlap_table_hits$name))) +
+
+  scale_y_continuous(
+    labels = nhmmscan_hits_filtered_merged_noOverlap_table_hits_qLens$shortName,
+    breaks = 1:nrow(nhmmscan_hits_filtered_merged_noOverlap_table_hits_qLens)
+  ) +
+  sofonias_theme
+```
+
+
+```{r}
+#| column: screen-inset-shaded
+create_dt(nhmmscan_hits_filtered_merged_noOverlap_table_hits)
+```
+
+
+```{r}
+#| column: screen-inset-shaded
+#| fig-height: !expr 'nrow(nhmmscan_hits_filtered_merged_noOverlap_table_hits) * 0.1'
+ggplotly(domainPlot)
+```
+)";
 
 
 int programWrapperRunner::runnhmmscan(const njh::progutils::CmdArgs & inputCommands){
@@ -341,6 +507,11 @@ int programWrapperRunner::runnhmmscan(const njh::progutils::CmdArgs & inputComma
 		for(const auto & name : seqsWithNoDomains){
 			noDomainHits << name << std::endl;
 		}
+	}
+
+	{
+		OutputStream qmdPlotDomainsOut(njh::files::make_path(setUp.pars_.directoryName_, "plotDomainsHits.qmd"));
+		qmdPlotDomainsOut << plotDomainsQmd << std::endl;
 	}
 
 	return 0;
