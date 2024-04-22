59ce0ca0217b5099280c4471c07b404abffed7ba
Nicholas Hathaway <nickjhathaway@gmail.com>
2024-04-22 11:47:04 -0400

    adding upgrade scripts;

---
+++ upgradeDevelop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+	echo "Need to supply number of CPUs to use, e.g. ./upgradeDevelop.sh 7"
+	exit
+fi
+
+#make sure on the master branch and then pull for any new commits
+git checkout develop
+git pull
+#re-run install
+./configure.py
+rm -fr external/build/bamtools/ external/build/jsoncpp/ external/build/restbed/ external/build/hts/
+rm -fr external/local/bamtools/ external/local/jsoncpp/ external/local/restbed/ external/local/hts/
+rm -fr external/local/pathweaver/ external/local/seekdeep/ external/local/njhseq/ external/local/seqServer/ external/local/TwoBit/ external/local/njhcpp/
+
+./setup.py --compfile compfile.mk --outMakefile makefile-common.mk --overWrite
+
+make clean
+
+make -j $1
