d84bac1ad06faf4c53ee0de49d6c13f63353bcae
Nicholas Hathaway <nickjhathaway@gmail.com>
2024-10-23 08:50:42 -0700

    no message

--- src/elucidatorPrograms/genExp/genExp_profileErrorsReadsToReferenceSeq.cpp
+++ src/elucidatorPrograms/genExp/genExp_profileErrorsReadsToReferenceSeq.cpp
@@ -6,7 +6,7 @@
  */
 
 
-
+#include <njhseq/seqToolsUtils/tandemRepeatUtils.hpp>
 
 #include "genExp.hpp"
 #include "elucidator/simulation/SeqTechSimulation/Illumina/RoughIlluminaProfiler.hpp"
@@ -15,17 +15,28 @@
 namespace njhseq {
 
 
-int genExpRunner::profileErrorsReadsToReferenceSeq(const njh::progutils::CmdArgs & inputCommands){
-
+int genExpRunner::profileErrorsReadsToReferenceSeq(const njh::progutils::CmdArgs & inputCommands) {
 	seqInfo refInfo("ref");
-
+	bool countHomopolymerErrorRates = false;
+	uint32_t minHomopolymerSize = 4;
+	bool countSimpleTandemRepeatSize = false;
+	uint32_t minSimpleTandemRepeatSize = 2;
+	uint32_t maxSimpleTandemRepeatSize = 3;
 	seqSetUp setUp(inputCommands);
 	setUp.processVerbose();
 	setUp.processDebug();
+
+	setUp.setOption(countHomopolymerErrorRates, "--countHomopolymerErrorRates", "count Homopolymer Error Rates");
+	setUp.setOption(minHomopolymerSize, "--minHomopolymerSize", "min Homopolymer Size");
+	setUp.setOption(countSimpleTandemRepeatSize, "--countSimpleTandemRepeatSize", "count Simple Tandem Repeat Size");
+	setUp.setOption(minSimpleTandemRepeatSize, "--minSimpleTandemRepeatSize", "min Simple Tandem Repeat Size");
+	setUp.setOption(maxSimpleTandemRepeatSize, "--maxSimpleTandemRepeatSize", "max Simple Tandem Repeat Size");
+
+
 	setUp.processSeq(refInfo, "--ref", "Reference sequence to compare to", true);
-  setUp.processReadInNames();
-  setUp.processDirectoryOutputName(true);
-  setUp.processAlnInfoInput();
+	setUp.processReadInNames();
+	setUp.processDirectoryOutputName(true);
+	setUp.processAlnInfoInput();
 	setUp.finishSetUp(std::cout);
 
 	setUp.startARunLog(setUp.pars_.directoryName_);
@@ -66,6 +77,16 @@ int genExpRunner::profileErrorsReadsToReferenceSeq(const njh::progutils::CmdArgs
 	errorCounter.writeProfiles(njh::files::make_path(setUp.pars_.directoryName_, "all").string(), true);
 	errorCounter.writeIndels(njh::files::make_path(setUp.pars_.directoryName_, "all").string(), true);
 
+	if(countHomopolymerErrorRates || countSimpleTandemRepeatSize) {
+		SimpleTandemRepeatFinder::SimpleTRFinderLocsPars pars;
+		if(countHomopolymerErrorRates) {
+			pars.minRepeatUnitSize = 1;
+		} else {
+			pars.minRepeatUnitSize = minSimpleTandemRepeatSize;
+			pars.maxRepeatUnitSize = maxSimpleTandemRepeatSize;
+		}
+
+	}
 
 	alignerObj.processAlnInfoOutputNoCheck(setUp.pars_.outAlnInfoDirName_, setUp.pars_.verbose_);
 
